f4d96c24e4a5196fc46d2dc85e10338441b370e7
shixiangwen <shixiangwen1@huya.com>
2023-08-17 17:49:46 +0800

    update

--- spacemesh_monitor.py
+++ spacemesh_monitor.py
@@ -91,7 +91,7 @@ def get_node_info(public_port, private_port):
 
         post_result = call_grpc(private_port, "spacemesh.v1.SmesherService.PostSetupStatus")
         post_state = post_result["status"]["state"]
-        num_labels_written = post_result["status"]["numLabelsWritten"]
+        num_labels_written = post_result["status"].get("numLabelsWritten", 0)
         data_dir = post_result["status"]["opts"]["dataDir"]
         num_units = post_result["status"]["opts"]["numUnits"]
         max_filesize = post_result["status"]["opts"]["maxFileSize"]
