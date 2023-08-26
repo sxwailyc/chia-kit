de1be32f7dfdbeb98ab9f4a6bd99b9663d27e135
shixiangwen <shixiangwen1@huya.com>
2023-08-26 08:10:05 +0800

    update

--- test.py
+++ test.py
@@ -18,7 +18,7 @@ import select
 def test():
     cmd = os.path.join(os.path.join(os.path.dirname(__file__), "bin"), "grpcurl")
     data = {}
-    service = "spacemesh.v1.AdminService.EventsStream "
+    service = "spacemesh.v1.AdminService.EventsStream"
     p = subprocess.Popen([cmd, '--plaintext', '-d', json.dumps(data), '127.0.0.1:9096', service], stdout=subprocess.PIPE)
     poll_obj = select.poll()
     poll_obj.register(p.stdout, select.POLLIN)
