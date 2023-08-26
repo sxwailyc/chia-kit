c69d58c7cee12694853cef41e3d56013e7d66bfa
shixiangwen <shixiangwen1@huya.com>
2023-08-26 11:45:38 +0800

    update

--- spacemesh_config_generator.py
+++ spacemesh_config_generator.py
@@ -19,8 +19,20 @@ def log(msg):
     s = "[%s]%s" % (datetime.now().strftime("%Y-%m-%d %H:%M:%S"), msg)
     print(s)
 
+boot_nodes = """"bootnodes": [
+      "/dns4/mainnet-bootnode-0.spacemesh.network/tcp/5000/p2p/12D3KooWPStnitMbLyWAGr32gHmPr538mT658Thp6zTUujZt3LRf",
+      "/dns4/mainnet-bootnode-2.spacemesh.network/tcp/5000/p2p/12D3KooWAsMgXLpyGdsRNjHBF3FaXwnXhyMEqWQYBXUpvCHNzFNK",
+      "/dns4/mainnet-bootnode-4.spacemesh.network/tcp/5000/p2p/12D3KooWRcTWDHzptnhJn5h6CtwnokzzMaDLcXv6oM9CxQEXd5FL",
+      "/dns4/mainnet-bootnode-6.spacemesh.network/tcp/5000/p2p/12D3KooWRS47KAs3ZLkBtE2AqjJCwxRYqZKmyLkvombJJdrca8Hz",
+      "/dns4/mainnet-bootnode-8.spacemesh.network/tcp/5000/p2p/12D3KooWFYv99aGbtXnZQy6UZxyf72NpkWJp3K4HS8Py35WhKtzE",
+      "/dns4/mainnet-bootnode-10.spacemesh.network/tcp/5000/p2p/12D3KooWHK5m83sNj2eNMJMGAngcS9gBja27ho83t79Q2CD4iRjQ",
+      "/dns4/mainnet-bootnode-12.spacemesh.network/tcp/5000/p2p/12D3KooWG4gk8GtMsAjYxHtbNC7oEoBTMRLbLDpKgSQMQkYBFRsw",
+      "/dns4/mainnet-bootnode-14.spacemesh.network/tcp/5000/p2p/12D3KooWRkZMjGNrQfRyeKQC9U58cUwAfyQMtjNsupixkBFag8AY",
+      "/dns4/mainnet-bootnode-16.spacemesh.network/tcp/5000/p2p/12D3KooWDAFRuFrMNgVQMDy8cgD71GLtPyYyfQzFxMZr2yUBgjHK",
+      "/dns4/mainnet-bootnode-18.spacemesh.network/tcp/5000/p2p/12D3KooWMJmdfwxDctuGGoTYJD8Wj9jubQBbPfrgrzzXaQ1RTKE6"
+    ],"""
 
-def generate(output_dir, start_port):
+def generate(output_dir, start_port, direct_port=7514, direct_id=None):
     """generator config"""
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
@@ -37,6 +49,18 @@ def generate(output_dir, start_port):
         content = content.replace('${grpc-public-listener}', '%s' % grpc_public_listener)
         content = content.replace('${grpc-private-listener}', '%s' % grpc_private_listener)
         content = content.replace('${grpc-json-listener}', '%s' % grpc_json_listener)
+
+        if direct_id:
+            node_settings = f""""disable-dht": true,
+    "bootnodes": [],
+    "direct": [
+        "/ip4/0.0.0.0/tcp/{direct_port}/p2p/{direct_id}"
+    ],"""
+        else:
+            node_settings = boot_nodes
+
+        content = content.replace('${node-settings}', '%s' % node_settings)
+
         target = os.path.join(output_dir, "config.mainnet.json")
         with open(target, "w") as of:
             of.write(content)
@@ -48,18 +72,21 @@ def is_root():
 
 
 if __name__ == '__main__':
-    if not is_root():
-        print("must run by root")
-        sys.exit(0)
 
     parser = argparse.ArgumentParser(description="""
        This script is for monitor the harvester server and report to the server.
     """)
-    parser.add_argument("-o", "--output-dir", metavar="", help="output dir", default='')
+    parser.add_argument("-o", "--output-dir", metavar="", help="output dir", default='./')
     parser.add_argument("-p", "--start-port", metavar="", type=int, help="start port, default is 9092",
                         default=9092)
+    parser.add_argument("--direct-port", metavar="", type=int, help="direct port, default is 0",
+                        default=0)
+    parser.add_argument("--direct-id", metavar="", help="direct id, default is empty",
+                        default=0)
 
     args = parser.parse_args()
     output_dir = args.output_dir
     start_port = args.start_port
-    generate(output_dir, start_port)
+    direct_port = args.direct_port
+    direct_id = args.direct_id
+    generate(output_dir, start_port, direct_port=direct_port, direct_id=direct_id)
