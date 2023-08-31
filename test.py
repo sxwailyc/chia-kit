2210d16c49d261194cfe0bd4712a2efdcc7a7272
shixiangwen <shixiangwen1@huya.com>
2023-08-31 16:44:35 +0800

    update

--- test.py
+++ test.py
@@ -15,16 +15,17 @@ def test():
     cmd = os.path.join(os.path.join(os.path.dirname(__file__), "bin"), "grpcurl")
     data = {}
     service = "spacemesh.v1.AdminService.EventsStream"
-    p = subprocess.Popen([cmd, '--plaintext', '-d', json.dumps(data), '127.0.0.1:9096', service], stdout=subprocess.PIPE)
 
     ON_POSIX = 'posix' in sys.builtin_module_names
+
+    p = subprocess.Popen([cmd, '--plaintext', '-d', json.dumps(data), '127.0.0.1:9096', service], stdout=subprocess.PIPE, bufsize=1, close_fds=ON_POSIX)
 
     def enqueue_output(out, queue):
         for line in iter(out.readline, b''):
             queue.put(line)
         out.close()
 
-    p = Popen(['myprogram.exe'], stdout=PIPE, bufsize=1, close_fds=ON_POSIX)
+    ##p = Popen(['myprogram.exe'], stdout=PIPE, bufsize=1, close_fds=ON_POSIX)
     q = Queue()
     t = Thread(target=enqueue_output, args=(p.stdout, q))
     t.daemon = True  # thread dies with the program
