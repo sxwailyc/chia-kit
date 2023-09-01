18a38cca044598eccea1b90923c790d8b5a87138
shixiangwen <shixiangwen1@huya.com>
2023-09-01 20:27:38 +0800

    update

--- test.py
+++ test.py
@@ -44,7 +44,7 @@ def test():
             break
             #print('no output yet')
         else:  # got line
-            line = line.encode("utf8")
+            line = line.decode("utf8")
             print(line)
 
 
