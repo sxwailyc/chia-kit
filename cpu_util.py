878a004ecade5b952a453045ed006aa62a261304
shixiangwen <shixiangwen1@huya.com>
2024-04-03 16:02:39 +0800

    update

--- cpu_util.py
+++ cpu_util.py
@@ -4,7 +4,19 @@
 import psutil
 
 
+def get_cpu_model():
+    with open('/proc/cpuinfo') as fd:
+        for line in fd:
+            if line.startswith('model name'):
+                cpu_model = line.split(':')[1].strip().split()
+                cpu_model = cpu_model[0] + ' ' + cpu_model[2] + ' ' + cpu_model[-1]
+                return cpu_model
+    return 'unknow'
+
+
 def get_temperature():
+    cpu_model = get_cpu_model()
+    print(cpu_model)
     temperatures = psutil.sensors_temperatures()
     for key, items in temperatures.items():
         print(key)
