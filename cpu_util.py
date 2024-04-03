40d330031d5099614e03abd6999951cd2fe0b0e0
shixiangwen <shixiangwen1@huya.com>
2024-04-03 16:11:12 +0800

    update

--- cpu_util.py
+++ cpu_util.py
@@ -13,15 +13,26 @@ def get_cpu_model():
     return 'unknow'
 
 
+def get_cpu_temperature_key(cpu_model):
+    if cpu_model.startswith("Intel(R) Xeon(R)"):
+        return 'coretemp', 'Package id 0'
+    if cpu_model.startswith("AMD Ryzen"):
+        return 'k10temp', 'Tctl'
+
+
 def get_temperature():
     cpu_model = get_cpu_model()
-    print(cpu_model)
+    top_key, second_key = get_cpu_temperature_key(cpu_model)
     temperatures = psutil.sensors_temperatures()
     for key, items in temperatures.items():
-        print(key)
-        #print(items, type(items))
+        if key != 'top_key':
+            continue
+
         for item in items:
-            print(item, type(item))
+            if item.label == second_key:
+                return item.current
+
+    return 0
 
 
 if __name__ == '__main__':
