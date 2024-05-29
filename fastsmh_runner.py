7d05e064abdbd7ce9c3b2cc3110efa27d661bba0
shixiangwen <shixiangwen1@huya.com>
2024-05-29 11:15:18 +0800

    update

--- fastsmh_runner.py
+++ fastsmh_runner.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import threading
+
 import json
 import shutil
 import os
 import argparse
 import sys
+import time
 from datetime import datetime
 
 from subprocess import Popen, PIPE
 
+current_folder = None
 
 GB = 1024 * 1024 * 1024
 
@@ -24,11 +28,34 @@ def get_free(disk):
     return free
 
 
-def format_number(i):
-    if i <= 9:
-        return '0%s' % i
-    return i
+def is_interrupt(folder):
+    metadata = os.path.join(folder, "postdata_metadata.json")
+    if not os.path.exists(metadata):
+        return False, 0
+    with open(metadata) as f:
+        data = json.load(f)
+        NumUnits = data["NumUnits"]
+        MaxFileSize = data["MaxFileSize"]
+        last_file_idx = (NumUnits * 64 * GB / MaxFileSize - 1)
+        last_file = f"postdata_{last_file_idx}.bin"
+        if not os.path.exists(last_file) or os.path.getsize(last_file) < MaxFileSize:
+            return True, NumUnits
+    return False, 0
 
+
+def rename_plot(folder):
+    key_bin = os.path.join(folder, "key.bin")
+    with open(key_bin) as f:
+        s = f.read()
+        key = s[64:]
+        target = os.path.join(os.path.dirname(folder), f'post_{key}')
+        shutil.move(folder, target)
+
+def print_speed():
+    global current_folder
+    while True:
+        print(current_folder)
+        time.sleep(20)
 
 class FastsmhRunner:
 
@@ -37,37 +64,25 @@ class FastsmhRunner:
         self.numUnits = numUnits
         self.nonces = nonces
         self.bin = os.path.join(os.path.join(os.path.dirname(__file__), "bin"), "fastsmh")
-
-    def is_interrupt(self, folder):
-        metadata = os.path.join(folder, "postdata_metadata.json")
-        if not os.path.exists(metadata):
-            return False, 0
-        with open(metadata) as f:
-            data = json.load(f)
-            NumUnits = data["NumUnits"]
-            MaxFileSize = data["MaxFileSize"]
-            last_file_idx = (NumUnits * 64 * GB / MaxFileSize - 1)
-            last_file = f"postdata_{last_file_idx}.bin"
-            if not os.path.exists(last_file) or os.path.getsize(last_file) < MaxFileSize:
-                return True, NumUnits
-        return False, 0
+        self.current_folder = None
 
     def restart_interrupt_plot(self):
         for folder in self.folders:
             sub_folders = os.listdir(folder)
             for sub_folder in sub_folders:
                 sub_folder = os.path.join(folder, sub_folder)
-                is_interrupt, num_units = self.is_interrupt(sub_folder)
-                if is_interrupt:
-                    self.numUnits = num_units
+                interrupt, num_units = is_interrupt(sub_folder)
+                if interrupt:
                     log(f"continue interrupt plot: {sub_folder}, num_units: {num_units}")
-                    self.plot(sub_folder)
+                    self.plot(sub_folder, num_units)
 
-    def plot(self, folder):
-        cmd = f"{self.bin} -datadir {folder} -nonces {self.nonces} -numUnits {self.numUnits}"
+    def plot(self, folder, num_units):
+        global current_folder
+        current_folder = folder
+        cmd = f"{self.bin} -datadir {folder} -nonces {self.nonces} -numUnits {num_units}"
         log(cmd)
         os.environ['LD_LIBRARY_PATH'] = f"{os.path.join(os.path.dirname(__file__), 'bin')}/:{os.environ['LD_LIBRARY_PATH']}"
-        p = Popen([self.bin, "-datadir", folder, "-nonces", f"{self.nonces}", "-numUnits", f"{self.numUnits}"], stdout=PIPE)
+        p = Popen([self.bin, "-datadir", folder, "-nonces", f"{self.nonces}", "-numUnits", f"{num_units}"], stdout=PIPE)
         while True:
             line = p.stdout.readline()
             if not line:
@@ -81,15 +96,23 @@ class FastsmhRunner:
                 continue
 
             for i in range(10):
-                sub_dir = os.path.join(folder, f"post_{format_number(i + 1)}")
+                sub_dir = os.path.join(folder, f"post_{i + 1}")
                 if os.path.exists(sub_dir):
                     continue
                 else:
                     log(f"create dir{sub_dir}")
                     os.makedirs(sub_dir)
-                    self.plot(sub_dir)
+                    self.plot(sub_dir, self.numUnits)
+                    rename_plot(sub_dir)
+
+                    free = get_free(folder)
+                    if free < self.numUnits * 64 * GB:
+                        break
 
     def start(self):
+
+        t = threading.Thread(target=print_speed)
+        t.start()
 
         self.restart_interrupt_plot()
 
@@ -103,7 +126,7 @@ if __name__ == '__main__':
     """)
 
     parser.add_argument("--num-units", metavar="", type=int, help="numUnits, default is 32", default=32)
-    parser.add_argument("--nonces", metavar="", type=int, help="nonces, default is 288", default=32)
+    parser.add_argument("--nonces", metavar="", type=int, help="nonces, default is 288", default=288)
     parser.add_argument("-d", "--dir", nargs='+', action='append', help="plot dirs")
 
     args = parser.parse_args()
