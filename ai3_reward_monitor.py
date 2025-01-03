77e5b3d5e4ac32db2b184e9cba9299299cbb7b0b
shixiangwen <114476513@qq.com>
2025-01-03 17:48:42 +0800

    update

--- ai3_reward_monitor.py
+++ ai3_reward_monitor.py
@@ -12,7 +12,13 @@ from watchdog.events import FileSystemEventHandler
 FARM_ID_KEY = "subspace_farmer::commands::farm:   ID:"
 FARM_SIZE_KEY = "subspace_farmer::commands::farm:   Allocated space:"
 FARM_DIRECTORY_KEY = "subspace_farmer::commands::farm:   Directory:"
+
+CLUSTER_FARM_ID_KEY = "subspace_farmer::commands::cluster::farmer:   ID:"
+CLUSTER_FARM_SIZE_KEY = "subspace_farmer::commands::cluster::farmer:   Allocated space:"
+CLUSTER_FARM_DIRECTORY_KEY = "subspace_farmer::commands::cluster::farmer:   Directory:"
+
 SUCCESS_REWARD_KEY = "Successfully signed reward hash"
+
 
 GB = 1024 * 1024 * 1024
 TB = GB * 1024
@@ -55,35 +61,35 @@ def get_farm_index(s):
     return farm_index
 
 
-def get_farm_id(s):
-    start = s.find(FARM_ID_KEY)
+def get_farm_id(s, keyword):
+    start = s.find(keyword)
     if start > 0:
-        farm_id = s[start + len(FARM_ID_KEY):]
+        farm_id = s[start + len(keyword):]
         farm_index = get_farm_index(s)
         return farm_index, farm_id.strip()
     return 0, None
 
 
-def get_farm_size(s):
-    start = s.find(FARM_SIZE_KEY)
+def get_farm_size(s, keyword):
+    start = s.find(keyword)
     end = s.rfind("(")
     if start > 0:
-        farm_size = s[start + len(FARM_SIZE_KEY):end]
+        farm_size = s[start + len(keyword):end]
         return format_size(farm_size)
     return 0
 
 
-def get_farm_directory(s):
-    start = s.find(FARM_DIRECTORY_KEY)
+def get_farm_directory(s, keyword):
+    start = s.find(keyword)
     if start > 0:
-        directory = s[start + len(FARM_DIRECTORY_KEY):]
+        directory = s[start + len(keyword):]
         return directory.strip()
     return None
 
 
 class Ai3RewardHandler(FileSystemEventHandler):
 
-    def __init__(self, secret, path):
+    def __init__(self, secret, path, cluster):
         FileSystemEventHandler.__init__(self)
         self.secret = secret
         self.hostname = socket.gethostname()
@@ -92,6 +98,9 @@ class Ai3RewardHandler(FileSystemEventHandler):
         self.current_farm_id = None
         self.current_farm_index = 0
         self.current_allocated = 0
+        self.farm_id_keyword = CLUSTER_FARM_ID_KEY if cluster else FARM_ID_KEY
+        self.farm_size_keyword = CLUSTER_FARM_SIZE_KEY if cluster else FARM_SIZE_KEY
+        self.farm_directory_keyword = CLUSTER_FARM_DIRECTORY_KEY if cluster else FARM_DIRECTORY_KEY
 
     def report_farm(self, directory):
         data = json.dumps({
@@ -138,12 +147,12 @@ class Ai3RewardHandler(FileSystemEventHandler):
             try_times += 1
 
     def handle_line(self, line):
-        if line.find(FARM_ID_KEY) > 0:
-            self.current_farm_index, self.current_farm_id = get_farm_id(line)
-        elif line.find(FARM_SIZE_KEY) > 0:
-            self.current_allocated = get_farm_size(line)
-        elif line.find(FARM_DIRECTORY_KEY) > 0:
-            directory = get_farm_directory(line)
+        if line.find(self.farm_id_keyword) > 0:
+            self.current_farm_index, self.current_farm_id = get_farm_id(line, self.farm_id_keyword)
+        elif line.find(self.farm_size_keyword) > 0:
+            self.current_allocated = get_farm_size(line, self.farm_size_keyword)
+        elif line.find(self.farm_directory_keyword) > 0:
+            directory = get_farm_directory(line, self.farm_directory_keyword)
             self.report_farm(directory)
         elif line.find(SUCCESS_REWARD_KEY) > 0:
             farm_index, reward_hash = get_reward_hash(line)
@@ -179,10 +188,12 @@ if __name__ == "__main__":
         """)
     parser.add_argument("--secret", metavar="", help="secret, use to post to server ")
     parser.add_argument("--log-path", metavar="", help="log path, use to watch farm event ")
+    parser.add_argument("--cluster", action="store_true", help="ai3 whether run on cluster mode, default is False", default=False)
 
     args = parser.parse_args()
     secret = args.secret
     log_path = args.log_path
+    cluster = args.cluster
     if not secret:
         print("please input secret with --secret")
         sys.exit(0)
@@ -191,9 +202,9 @@ if __name__ == "__main__":
         print("please input log_path with --log-path")
         sys.exit(0)
 
-    log(f'log path {secret}')
+    log(f'log path {log_path}')
 
-    event_handler = Ai3RewardHandler(secret, log_path)
+    event_handler = Ai3RewardHandler(secret, log_path, cluster)
     observer = Observer()
     observer.schedule(event_handler, log_path, recursive=False)
     observer.start()
